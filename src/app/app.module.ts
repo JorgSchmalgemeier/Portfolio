be27043b82df806ef5ec17ff3308b7c1dbbea4bd
Jörg Schmalgemeier <j.schmalgemeier@googlemail.com>
2023-06-13 15:04:27 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroductionComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
