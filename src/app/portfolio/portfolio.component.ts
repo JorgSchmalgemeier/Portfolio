44b8b4a9eab026cfd07467ca8c947cef5e6d19b3
Jörg Schmalgemeier <j.schmalgemeier@googlemail.com>
2024-02-06 22:07:42 +0100

    added new project images and adjusted the position for the images

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -10,7 +10,7 @@ export class PortfolioComponent {
   public projects = [
     {
       name: 'DA Bubble',
-      img: 'dabubble-img2.png',
+      img: 'dabubble-project1.png',
       tec: 'Angular | TypeScript | Firebase | HTML | SCSS',
       description: 'Chat-Messenger application in the style of Slack for team communication. You can create channels or chat directly with your co-workers.',
       githubLink: 'https://github.com/JorgSchmalgemeier/DABubble',
@@ -18,7 +18,7 @@ export class PortfolioComponent {
     },
     {
       name: 'Simple CRM',
-      img: 'crm-img.png',
+      img: 'crm-project1.png',
       tec: 'Angular | TypeScript | Firebase | HTML | SCSS',
       description: 'A simple customer relationship management software. Shows company data in graphs and gives the option to save customer information and product reviews.',
       githubLink: 'https://github.com/JorgSchmalgemeier/Simple-CRM',
@@ -26,7 +26,7 @@ export class PortfolioComponent {
     },
     {
       name: 'Join',
-      img: 'join-img2.png',
+      img: 'join-project1.png',
       tec: 'JavaScript | HTML | CSS',
       description: 'Task manager inspired by the kanban system. Create and organize tasks using drag and drop functions, assign users and categorys.',
       githubLink: 'https://github.com/JorgSchmalgemeier/Join',
@@ -34,7 +34,7 @@ export class PortfolioComponent {
     },
     {
       name: 'El Pollo Loco',
-      img: 'pollo-loco-img.png',
+      img: 'pollo-loco-project1.png',
       tec: 'JavaScript | HTML | CSS',
       description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and salsa bottles to fight against the crazy hen.',
       githubLink: 'https://github.com/JorgSchmalgemeier/El-Pollo-Loco',
